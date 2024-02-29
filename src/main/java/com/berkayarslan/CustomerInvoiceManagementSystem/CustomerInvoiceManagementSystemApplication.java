4b3fa0330e5d321219562cb4d55a712bea9c29b4
Berkay Arslan <berkayarslan9@gmail.com>
2024-02-29 13:40:41 +0300

    first commit

---
+++ src/main/java/com/berkayarslan/CustomerInvoiceManagementSystem/CustomerInvoiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.berkayarslan.CustomerInvoiceManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInvoiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInvoiceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkayarslan/CustomerInvoiceManagementSystem/CustomerInvoiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkayarslan.CustomerInvoiceManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInvoiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
