8611fb186f4eebdc2d991fe4c8119b93a4cffceb
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-02 15:32:12 +0300

    update Sector

---
+++ src/main/java/com/berkayarslan/CustomerInvoiceManagementSystem/model/Sector.java
@@ -0,0 +1,28 @@
+package com.berkayarslan.CustomerInvoiceManagementSystem.model;
+
+public enum Sector {
+    Education,
+    Mining,
+    Construction,
+    Entertainment,
+    Agriculture,
+    Hospitality,
+    Manufacturing,
+    Retail,
+    Aerospace,
+    Energy,
+    Media,
+    Transportation,
+    Advertising,
+    Computer,
+    Insurance,
+    Tourism,
+    Automobile,
+    Fashion,
+    Finance,
+    Pharmaceutical,
+    Telecommunication,
+    Health,
+    Technologies,
+    Technology, Food, Healthcare, Accommodation
+}
